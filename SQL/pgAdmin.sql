c7a7187597ae9d64b8a37580f1334645e0bf8636
renatabg2205 <75342959+renatabg2205@users.noreply.github.com>
2021-05-12 20:08:54 -0500

    Updating pgAdmin.sql

--- SQL/pgAdmin.sql
+++ SQL/pgAdmin.sql
@@ -40,30 +40,74 @@ CREATE TABLE uscounty (
 	avg_precip NUMERIC
 );
 
+CREATE TABLE weather_mapscaled (
+	winery_search_term TEXT NOT NULL,
+	lat INT NOT NULL,
+	lng INT NOT NULL,
+	avg_temp NUMERIC,
+	avg_airp NUMERIC,
+	avg_humid NUMERIC,
+	avg_precip NUMERIC,
+	country TEXT NOT NULL,
+	points INT,
+	variety TEXT,
+	winery TEXT NOT NULL
+);
+
+
 DROP TABLE wine;
 DROP TABLE weather;
 DROP TABLE machine;
 DROP TABLE uscounty;
 
 SELECT * FROM wine;
+---- 55446 rows
 
 SELECT * FROM weather;
+---- 7892 rows
 
 SELECT * FROM machine;
----- 105812 rows
+---- 52906 rows
 
 SELECT * FROM uscounty;
+---- 1797 rows
+
+SELECT * FROM weather_mapscaled;
+---- 52906 rows
 
 SELECT winery, country, points, variety, winery_search, avg_temp, avg_airp, avg_humid, avg_precip
 FROM wine AS wi, weather AS we 
-WHERE wi.winery_country = we.winery_search
----- 105812 rows
+WHERE wi.winery_country = we.winery_search;
+---- 52906 rows
+
+SELECT winery, country, points, variety, winery_search, avg_temp, avg_airp, avg_humid, avg_precip
+FROM weather AS we, wine AS wi  
+WHERE we.winery_search = wi.winery_country;
+---- 52906 rows
 
 SELECT winery, country, points, variety, winery_search, avg_temp, avg_airp, avg_humid, avg_precip
 FROM wine
 INNER JOIN weather 
 ON (wine.winery_country = weather.winery_search);
----- 105812 rows
+---- 52906 rows
+
+SELECT winery, country, points, variety, winery_search, avg_temp, avg_airp, avg_humid, avg_precip
+FROM wine
+LEFT JOIN weather 
+ON (wine.winery_country = weather.winery_search);
+---- 55446 rows
+
+SELECT winery, country, points, variety, winery_search, avg_temp, avg_airp, avg_humid, avg_precip
+FROM wine
+RIGHT JOIN weather 
+ON (wine.winery_country = weather.winery_search);
+----  52906 rows
+
+SELECT winery, country, points, variety, winery_search, avg_temp, avg_airp, avg_humid, avg_precip
+FROM wine
+FULL OUTER JOIN weather 
+ON (wine.winery_country = weather.winery_search);
+---- 5546 rows
 	
 SELECT DISTINCT winery FROM wine;
 
