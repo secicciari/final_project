9bfe38a7e4679f06afddfc1e9375710556169a97
renatabg2205 <75342959+renatabg2205@users.noreply.github.com>
2021-05-16 14:59:26 -0500

    Updating app.py

--- app/app.py
+++ app/app.py
@@ -1,11 +1,6 @@
 #Import tools
 import os, json
-from decimal import Decimal, localcontext
 from flask import Flask, render_template, redirect, jsonify
-from flask_migrate import Migrate
-#!/usr/bin/python
-import psycopg2
-import psycopg2.extras
 
 #Set up Flask
 app = Flask(__name__)
