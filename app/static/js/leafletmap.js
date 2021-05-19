8de8239886bf9688abf08975474339fff350149a
renatabg2205 <75342959+renatabg2205@users.noreply.github.com>
2021-05-19 11:53:04 -0500

    Updating leafletmap.js

--- app/static/js/leafletmap.js
+++ app/static/js/leafletmap.js
@@ -1,6 +1,9 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+// Import tools
+import os
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
