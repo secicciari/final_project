32a6d2e736d642d53c9aba6d2f6de2bb7256e047
renatabg2205 <75342959+renatabg2205@users.noreply.github.com>
2021-05-23 15:41:22 -0500

    Updating conditional.js and leafletmap.js

--- app/static/js/conditional.js
+++ app/static/js/conditional.js
@@ -6,31 +6,16 @@ if (location.hostname === "127.0.0.1") {
     s.async = true;
     s.src = '../static/js/config.js';
     h.appendChild(s);
-    console.log("index_working1");
-    console.log("s.src1");
-    console.log(s.src);
-    console.log("h.s.src1a");
-    console.log(h);
 
     var d1 = document, h1 = d1.getElementsByTagName('head')[0], s1 = d1.createElement('script');
     s1.type = 'text/javascript';
     s1.async = true;
     s1.src = '../static/js/leafletmap.js';
     h1.appendChild(s1);
-    console.log("index_working1b");
-    console.log("s1.src1b");
-    console.log(s1.src);
-    console.log("h.s.src1b");
-    console.log(h1);
 } else if (webpageurl == "https://green-team-wine.herokuapp.com/" || "http://green-team-wine.herokuapp.com/") {
     var d1 = document, h1 = d1.getElementsByTagName('head')[0], s1 = d1.createElement('script');
     s1.type = 'text/javascript';
     s1.async = true;
     s1.src = '../static/js/leafletmap.js';
     h1.appendChild(s1);
-    console.log("index_working1c");
-    console.log("s1.src1c");
-    console.log(s1.src);
-    console.log("h.s.src1c");
-    console.log(h1);
 }
